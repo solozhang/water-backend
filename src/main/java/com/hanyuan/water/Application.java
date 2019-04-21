5d2554af8e2f30efb2665f7f3ec41708d95b7eb0
zhangjiajia <zhangjiajia@tigerbrokers.com>
2019-04-21 18:37:04 +0800

    fix(file):mkdir

--- src/main/java/com/hanyuan/water/Application.java
+++ src/main/java/com/hanyuan/water/Application.java
@@ -16,6 +16,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
+        System.setProperty("file.encoding",    "UTF-8");
+        System.setProperty("sun.jnu.encoding", "UTF-8");
         SpringApplication.run(Application.class, args);
     }
 }
